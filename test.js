456823574897b6775279b10ba31f35b659a4ff43
DHGA2763 <152475524+DHGA2763@users.noreply.github.com>
2024-09-28 03:29:04 +0900

    Create test.js

---
+++ test.js
@@ -0,0 +1,31 @@
+import { system } from "@minecraft/server";
+import PageFormData from './pageFormData.js';
+
+system.afterEvents.scriptEventReceive.subscribe(event => {
+
+  const player = event?.sourceEntity
+  
+  if (event.id != 'pageForm:show') return;
+  if (player === undefined) return;
+
+  const form = new PageFormData(5);
+  form.title('titleText');
+  form.body('bodyText');
+
+  for ( let i = 0 ; i < 12 ; i ++ ) form.button('button' + i); 
+
+  form.show(player, (response) => {
+
+    if (response.canceled) {
+
+      console.warn('canceled');
+
+    } else {
+
+      console.warn('selection : ' + response?.selection);
+
+    };
+
+  });
+
+});
